5d8cec91c399474b469de45bc0bca09d8ac4b3c5
Nadeera_Binoli <95376457+NadeeraBinoli@users.noreply.github.com>
2023-10-24 02:18:37 +0530

    logo responsiveness and favicon

---
+++ index.js
@@ -0,0 +1,18 @@
+//  hamberger-menue button
+const navLinkUl = document.querySelector(".navList");
+const hambergerMenue = document.querySelector(".hamberger-menue");
+const menueClose = document.querySelector(".menue-close");
+
+hambergerMenue.addEventListener('click',()=> {
+const dataVisible =  navLinkUl.getAttribute("data-visible");
+if (dataVisible === "false") {
+  navLinkUl.setAttribute("data-visible", true);
+}
+}) ;
+
+menueClose.addEventListener('click',()=> {
+  const dataVisible =  navLinkUl.getAttribute("data-visible");
+ if (dataVisible==="true") {
+  navLinkUl.setAttribute("data-visible", false);
+}
+}) ;
